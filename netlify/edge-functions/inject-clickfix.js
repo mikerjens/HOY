33b4ffb07108632d955f8d32c934c7c9e17828ff
mikerjens <mk@fixer.fo>
2026-09-09 13:48:18 +0100

    Bust cache for Faroese credit message button

--- netlify/edge-functions/inject-clickfix.js
+++ netlify/edge-functions/inject-clickfix.js
@@ -9,7 +9,7 @@ export default async (request, context) => {
   html = html.replace(/<script src="\/credit-message-button\.js\?v=[^"]+"><\/script>/g, '');
   html = html.replace(/<script src="\/credit-route-fix\.js\?v=[^"]+"><\/script>/g, '');
   html = html.replace('</head>', '<style>.mine-note{display:none!important}</style></head>');
-  html = html.replace('</body>', '<script src="/clickfix.js?v=20260909-credit-spacing"></script><script src="/home-next-fix.js?v=20260909-next-event-2"></script><script src="/credit-message-button.js?v=20260909-credit-button-fo"></script><script src="/credit-route-fix.js?v=20260909-credit-route-click"></script></body>');
+  html = html.replace('</body>', '<script src="/clickfix.js?v=20260909-credit-spacing"></script><script src="/home-next-fix.js?v=20260909-next-event-2"></script><script src="/credit-message-button.js?v=20260909-credit-button-fo-2"></script><script src="/credit-route-fix.js?v=20260909-credit-route-click"></script></body>');
 
   const headers = new Headers(response.headers);
   headers.delete('content-length');
