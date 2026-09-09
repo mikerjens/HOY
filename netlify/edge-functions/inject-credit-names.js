5b0fc1fade885eae2725a6c8807d1935598787a3
mikerjens <mk@fixer.fo>
2026-09-09 14:37:13 +0100

    Bust cache for Danish credit confirmation label

--- netlify/edge-functions/inject-credit-names.js
+++ netlify/edge-functions/inject-credit-names.js
@@ -6,7 +6,7 @@ export default async (request, context) => {
   let html = await response.text();
   html = html.replace(/<script src="\/credit-names\.js\?v=[^"]+"><\/script>/g, '');
   html = html.replace(/<script src="\/credit-confirm\.js\?v=[^"]+"><\/script>/g, '');
-  html = html.replace('</body>', '<script src="/credit-names.js?v=20260909-rulletekst-names"></script><script src="/credit-confirm.js?v=20260909-confirm-credit"></script></body>');
+  html = html.replace('</body>', '<script src="/credit-names.js?v=20260909-rulletekst-names"></script><script src="/credit-confirm.js?v=20260909-confirm-credit-da"></script></body>');
 
   const headers = new Headers(response.headers);
   headers.delete('content-length');
