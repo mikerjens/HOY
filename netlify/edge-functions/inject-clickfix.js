a11f04866ceb6ac16688d87736461b52804cc886
mikerjens <mk@fixer.fo>
2026-09-11 13:12:32 +0100

    Remove redundant personal schedule sync layer

--- netlify/edge-functions/inject-clickfix.js
+++ netlify/edge-functions/inject-clickfix.js
@@ -12,7 +12,7 @@ export default async (request, context) => {
   html = html.replace(/<script src="\/vagtplan-late-sync\.js\?v=[^"]+"><\/script>/g, '');
   html = html.replace(/<script src="\/person-live-sync\.js\?v=[^"]+"><\/script>/g, '');
   html = html.replace('</head>', '<style>.mine-note{display:none!important}</style></head>');
-  html = html.replace('</body>', '<script src="/clickfix.js?v=20260909-stjorna5-fix"></script><script src="/home-next-fix.js?v=20260909-next-event-2"></script><script src="/credit-message-button.js?v=20260909-credit-button-fo-2"></script><script src="/credit-route-fix.js?v=20260909-credit-route-click"></script><script src="/person-live-sync.js?v=20260911-person-live-3"></script><script src="/mine-task-detail.js?v=20260911-task-detail-3"></script></body>');
+  html = html.replace('</body>', '<script src="/clickfix.js?v=20260909-stjorna5-fix"></script><script src="/home-next-fix.js?v=20260909-next-event-2"></script><script src="/credit-message-button.js?v=20260909-credit-button-fo-2"></script><script src="/credit-route-fix.js?v=20260909-credit-route-click"></script><script src="/mine-task-detail.js?v=20260911-task-detail-3"></script></body>');
 
   const headers = new Headers(response.headers);
   headers.delete('content-length');
